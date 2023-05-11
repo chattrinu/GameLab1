d538c7c11ba1ae656401495aa8377fc27a5b20e3
chattrinu <chattrinu@gmail.com>
2023-05-11 17:20:12 -0400

    submit2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args)  {
 
        Players player1 = new Players("Tony","scissors");
-       Players player2 = new Players("Emma","pepper");
+       Players player2 = new Players("Emma","rock");
        PlayGame game = new PlayGame();
        game.turn(player1,player2);
 
--- src/PlayGame.java
+++ src/PlayGame.java
@@ -1,44 +1,67 @@
 public class PlayGame {
     public Players player1, player2;
-
+    int point =0;
     public void turn(Players player1, Players player2)  {
 
             if (player1.getChoose() == "pepper" && player2.getChoose() == "pepper") {
-                System.out.println(player1.getName() + "and" + player2.getName() + "tie");
+                System.out.println(player1.getName()+" chose "+player1.getChoose()+" "
+                        +player2.getName()+" chose "+player2.getChoose());
+                System.out.println(player1.getName() + " and " + player2.getName() + " tie");
                 System.out.printf("%s points is %d \n", player1.getName(), player1.getPoint(0));
                 System.out.printf("%s points is %d ", player2.getName(), player2.getPoint(0));
             } else if (player1.getChoose() == "pepper" && player2.getChoose() == "rock") {
+                System.out.println(player1.getName()+" chose "+player1.getChoose()+" "
+                        +player2.getName()+" chose "+player2.getChoose());
                 System.out.println(player1.getName() + " is win ");
-                System.out.printf("%s points is %d \n", player1.getName(), player1.getPoint(1));
+                player1.setPoint(point++);
+                System.out.printf("%s points is %d \n", player1.getName(), player1.getPoint(point));
                 System.out.printf("%s points is %d ", player2.getName(), player2.getPoint(0));
             } else if (player1.getChoose() == "pepper" && player2.getChoose() == "scissors") {
+                System.out.println(player1.getName()+" chose "+player1.getChoose()+" "
+                        +player2.getName()+" chose "+player2.getChoose());
+                player2.setPoint(point++);
                 System.out.println(player2.getName() + " is win ");
                 System.out.printf("%s points is %d \n", player1.getName(), player1.getPoint(0));
-                System.out.printf("%s points is %d ", player2.getName(), player2.getPoint(1));
+                System.out.printf("%s points is %d ", player2.getName(), player2.getPoint(point));
             } else if (player1.getChoose() == "rock" && player2.getChoose() == "rock") {
-                System.out.println(player1.getName() + "and" + player2.getName() + "tie");
+                System.out.println(player1.getName()+" chose "+player1.getChoose()+" "
+                        +player2.getName()+" chose "+player2.getChoose());
+                System.out.println(player1.getName() + " and " + player2.getName() + " tie ");
                 System.out.printf("%s points is %d \n", player1.getName(), player1.getPoint(0));
                 System.out.printf("%s points is %d ", player2.getName(), player2.getPoint(0));
             } else if (player1.getChoose() == "rock" && player2.getChoose() == "pepper") {
+                System.out.println(player1.getName()+" chose "+player1.getChoose()+" "
+                        +player2.getName()+" chose "+player2.getChoose());
+                player2.setPoint(point++);
                 System.out.println(player2.getName() + " is win ");
                 System.out.printf("%s points is %d \n", player1.getName(), player1.getPoint(0));
-                System.out.printf("%s points is %d ", player2.getName(), player2.getPoint(1));
+                System.out.printf("%s points is %d ", player2.getName(), player2.getPoint(point));
             } else if (player1.getChoose() == "rock" && player2.getChoose() == "scissors") {
+                System.out.println(player1.getName()+" chose "+player1.getChoose()+" "
+                        +player2.getName()+" chose "+player2.getChoose());
                 System.out.println(player1.getName() + " is win ");
-                System.out.printf("%s points is %d \n", player1.getName(), player1.getPoint(1));
+                player1.setPoint(point++);
+                System.out.printf("%s points is %d \n", player1.getName(), player1.getPoint(point));
                 System.out.printf("%s points is %d ", player2.getName(), player2.getPoint(0));
             } else if (player1.getChoose() == "scissors" && player2.getChoose() == "scissors") {
-                System.out.println(player1.getName() + "and" + player2.getName() + "tie");
+                System.out.println(player1.getName()+" chose "+player1.getChoose()+" "
+                        +player2.getName()+" chose "+player2.getChoose());
+                System.out.println(player1.getName() + " and " + player2.getName() + " tie");
                 System.out.printf("%s points is %d \n", player1.getName(), player1.getPoint(0));
                 System.out.printf("%s points is %d ", player2.getName(), player2.getPoint(0));
             } else if (player1.getChoose() == "scissors" && player2.getChoose() == "pepper") {
+                System.out.println(player1.getName()+" chose "+player1.getChoose()+" "
+                        +player2.getName()+" chose "+player2.getChoose());
                 System.out.println(player1.getName() + " is win ");
-                System.out.printf("%s points is %d \n", player1.getName(), player1.getPoint(1));
+                player1.setPoint(point++);
+                System.out.printf("%s points is %d \n", player1.getName(), player1.getPoint(point));
                 System.out.printf("%s points is %d ", player2.getName(), player2.getPoint(0));
             } else if (player1.getChoose() == "scissors" && player2.getChoose() == "rock") {
-                System.out.println(player2.getName() + " is win ");
+                System.out.println(player1.getName()+" chose "+player1.getChoose()+" "
+                        +player2.getName()+" chose "+player2.getChoose());
+                player2.setPoint(point++);
                 System.out.printf("%s points is %d \n", player1.getName(), player1.getPoint(0));
-                System.out.printf("%s points is %d ", player2.getName(), player2.getPoint(1));
+                System.out.printf("%s points is %d ", player2.getName(), player2.getPoint(point));
             }
         }
     }
